ab269eea511ece63784a3ab4a9e87b475ece8de2
Kyle Carrero <kdcarrero@gmail.com>
2016-12-05 21:46:51 -0500

    Added support for the Picasso library to make the loading of images easier and more efficient in the recyclerviews.

--- app/src/main/java/com/snippet/snippet/controller/adapters/PhotosRecyclerViewAdapter.java
+++ app/src/main/java/com/snippet/snippet/controller/adapters/PhotosRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.snippet.snippet.controller.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.snippet.snippet.R;
-import com.snippet.snippet.controller.ImageViewOnClickListener;
+import com.snippet.snippet.controller.ImageUtils;
 
 import java.util.List;
 
@@ -18,8 +17,7 @@ import java.util.List;
  */
 
 public class PhotosRecyclerViewAdapter extends RecyclerView.Adapter<PhotosRecyclerViewAdapter.ViewHolder> {
-    private List<Bitmap> mDataset;
-    private List<Integer> mDatasetIds; // Ids for the images in the bitmaps (for launching image viewer on click)
+    private List<String> mDataset;
     private Context mContext;
 
     // Provide a reference to the views for each data item
@@ -36,32 +34,30 @@ public class PhotosRecyclerViewAdapter extends RecyclerView.Adapter<PhotosRecycl
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public PhotosRecyclerViewAdapter(List<Bitmap> myDataset, List<Integer> myDatasetIds, Context context) {
+    public PhotosRecyclerViewAdapter(List<String> myDataset, Context context) {
         mDataset = myDataset;
-        mDatasetIds = myDatasetIds;
         mContext = context;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
     public PhotosRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                                   int viewType) {
+                                                                          int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.image_view_preview, parent, false);
         // set the view's size, margins, paddings and layout parameters
 
-        ViewHolder vh = new ViewHolder((ImageView) v, mContext);
+        PhotosRecyclerViewAdapter.ViewHolder vh = new PhotosRecyclerViewAdapter.ViewHolder((ImageView) v, mContext);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(PhotosRecyclerViewAdapter.ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mImageView.setImageBitmap(mDataset.get(position));
-        holder.mImageView.setOnClickListener(new ImageViewOnClickListener(mContext, mDatasetIds.get(position)));
+        ImageUtils.addImageToImageView(mContext, holder.mImageView, mDataset.get(position), 150, 150);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -70,31 +66,8 @@ public class PhotosRecyclerViewAdapter extends RecyclerView.Adapter<PhotosRecycl
         return mDataset.size();
     }
 
-    public void addImage(Bitmap imageBitmap, int imageId) {
-        mDataset.add(imageBitmap);
-        mDatasetIds.add(imageId);
-        this.notifyDataSetChanged();
+    public void replaceDataset(List<String> dataset) {
+        mDataset = dataset;
+        notifyDataSetChanged();
     }
-
-    public void addImage(List<Bitmap> imageBitmaps, List<Integer> imageIds) {
-        for (Bitmap imageBitmap: imageBitmaps) {
-            mDataset.add(imageBitmap);
-        }
-        for (Integer imageId: imageIds) {
-            mDatasetIds.add(imageId);
-        }
-        this.notifyDataSetChanged();
-    }
-
-    public void addImage(Bitmap[] imageBitmaps, int[] imageIds) {
-        for (Bitmap imageBitmap: imageBitmaps) {
-            mDataset.add(imageBitmap);
-        }
-        for (Integer imageId: imageIds) {
-            mDatasetIds.add(imageId);
-        }
-        this.notifyDataSetChanged();
-    }
-
-
 }
