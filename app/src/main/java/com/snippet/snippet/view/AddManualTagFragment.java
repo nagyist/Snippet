9bc8152f795eb4be26a6ee5c22341faf2f2d2c2c
jsburklund <jsburklund@wpi.edu>
2016-12-09 17:35:38 -0500

    Add tag from auto complete box to temp list

--- app/src/main/java/com/snippet/snippet/view/AddManualTagFragment.java
+++ app/src/main/java/com/snippet/snippet/view/AddManualTagFragment.java
@@ -7,10 +7,18 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.snippet.snippet.R;
+import com.snippet.snippet.controller.adapters.TagAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Jordan Burklund
@@ -20,14 +28,19 @@ import com.snippet.snippet.R;
 public class AddManualTagFragment extends DialogFragment {
     Button addNewTagButton;
     Button addAllTagsButton;
+    AutoCompleteTextView tagText;
+    GridView tagsGridView;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstance) {
+        //Create a new tag adapter for the grid view to add and remove tags
+        List<String> testList = new ArrayList<String>(Arrays.asList("apple", "orange"));
+        final TagAdapter tagAdapter = new TagAdapter(getActivity(), new ArrayList<String>());
 
         //Use the builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        final View view = getActivity().getLayoutInflater().inflate(R.layout.add_tag_manual, null);;
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.add_tag_manual, null);
         builder.setView(view);
         builder.setPositiveButton("Add tags", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -40,6 +53,8 @@ public class AddManualTagFragment extends DialogFragment {
             }
         });
 
+        //Get a reference to the auto complete text view
+        tagText = (AutoCompleteTextView) view.findViewById(R.id.autoCompleteTextView);
 
         //Add a listener to the Add button to add the tag to the temp list
         addNewTagButton = (Button) view.findViewById(R.id.addNewTagButton);
@@ -47,8 +62,13 @@ public class AddManualTagFragment extends DialogFragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(), "Pressed Add Tag", Toast.LENGTH_SHORT).show();
+                tagAdapter.addTag(tagText.getText().toString());
             }
         });
+
+        //Add the adapter to the grid view
+        tagsGridView = (GridView) view.findViewById(R.id.fragmentTagGrid);
+        tagsGridView.setAdapter(tagAdapter);
 
         //Build the AlertDialog and return it;
         return builder.create();
